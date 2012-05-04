2ec9b5f273ed76ccbb93f88a365affa172870ab2
Philip Brockmeyer <PhilipBrockmeyer@gmail.com>
2012-05-03 22:42:28 -0600

    added the twitter bird icon as per the twitter design guidelines

--- src/BirdFeed/Properties/Settings.Designer.cs
+++ src/BirdFeed/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace BirdFeed.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("Wall-E")]
+        [global::System.Configuration.DefaultSettingValueAttribute("#MaybeIshould")]
         public string SearchTerm {
             get {
                 return ((string)(this["SearchTerm"]));
