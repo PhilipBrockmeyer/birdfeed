83d61530efaf3a85df3378b01f40341a56cf8e46
unknown <Phil@Desktop.(none)>
2012-05-03 18:43:42 -0600

    Created a circular queue

---
+++ src/BirdFeed/CircularQueue.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace BirdFeed
+{
+    public class CircularQueue<T> : IEnumerable<T>
+    {
+        public Int32 Capacity { get; private set; }
+        private Int32 _currentIndex;
+        private Int32 _insertionIndex;
+        private readonly CircularQueueEnumerator<T> _enumerator;
+        private readonly List<ItemWrapper<T>> _items;
+        public CircularQueue(Int32 capacity)
+        {
+
+            if (capacity < 1)
+                throw new ArgumentException("The capacity must be 1 or greater.", "capacity");
+            this._items = new List<ItemWrapper<T>>(capacity);
+            this._enumerator = new CircularQueueEnumerator<T>(this);
+            this._currentIndex = 0;
+            this._insertionIndex = 0;
+            this.Capacity = capacity;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+
+            return this._enumerator;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+
+            return this._enumerator;
+        }
+
+        public void Add(T item)
+        {
+
+            if (this._items.Count == 0)
+            {
+
+                this._items.Add(new ItemWrapper<T>(item));
+            }
+
+            else
+            {
+
+                this._insertionIndex++;
+                this._items.Insert(this._insertionIndex, new ItemWrapper<T>(item));
+            }
+
+        }
+
+        private void PruneItems()
+        {
+
+            if (this._items.Count <= this.Capacity)
+                return;
+            while (this._items.Count > this.Capacity)
+            {
+
+                var oldestItem = this._items.OrderBy(x => x.DateTime).First();
+                this._items.Remove(oldestItem);
+            }
+
+        }
+
+        private bool MoveNext()
+        {
+
+            if (this._items.Count == 0)
+                return false;
+            var currentItem = this._items[_currentIndex];
+            this.PruneItems();
+            var currentIndexAfterPrune = this._items.IndexOf(currentItem);
+            this._currentIndex = (currentIndexAfterPrune + 1) % Math.Min(this.Capacity, this._items.Count);
+            this._insertionIndex = this._currentIndex;
+            return true;
+        }
+
+
+
+        private void Reset()
+        {
+
+            this._currentIndex = 0;
+        }
+
+        private T GetCurrent()
+        {
+
+            if (this._items.Count == 0)
+                return default(T);
+            return this._items[this._currentIndex].Item;
+        }
+
+        private class ItemWrapper<T>
+        {
+
+            public DateTime DateTime { get; private set; }
+            public T Item { get; private set; }
+            public ItemWrapper(T item)
+            {
+
+                this.Item = item;
+                this.DateTime = DateTime.Now;
+            }
+        }
+
+        private class CircularQueueEnumerator<T> : IEnumerator<T>
+        {
+            private readonly CircularQueue<T> _parent;
+            public CircularQueueEnumerator(CircularQueue<T> parent)
+            {
+
+                this._parent = parent;
+            }
+
+            public T Current
+            {
+
+                get { return this._parent.GetCurrent(); }
+            }
+
+            public void Dispose() { }
+            object IEnumerator.Current
+            {
+
+                get { return this._parent.GetCurrent(); }
+            }
+
+            public bool MoveNext()
+            {
+
+                return this._parent.MoveNext();
+            }
+
+            public void Reset()
+            {
+
+                this._parent.Reset();
+            }
+        }
+    }
+}
+
---
+++ src/BirdFeed/TwitterFeedViewModel.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BirdFeed
+{
+    public class TwitterFeedViewModel : ViewModel
+    {
+
+        private CircularQueue<TweetViewModel> _queue;
+        private IEnumerator<TweetViewModel> _items;
+        private TwitterService _service;
+        private Int64 _lastId;
+        private Dispatcher _dispatcher;
+        public class TweetViewModel : ViewModel
+        {
+
+            private BitmapImage _userImage;
+            public BitmapImage UserImage
+            {
+
+                get { return _userImage; }
+                set { ChangeAndNotify(ref _userImage, value, () => UserImage); }
+            }
+
+            private readonly ObservableCollection<TextSegment> _text = new ObservableCollection<TextSegment>();
+            public ObservableCollection<TextSegment> Text { get { return _text; } }
+            private String _textSource;
+            public String TextSource
+            {
+
+                get { return _textSource; }
+                set { ChangeAndNotify(ref _textSource, value, () => TextSource); }
+            }
+
+            public void SetData(TwitterSearchStatus status)
+            {
+
+                Int32 entityIndex = 0;
+                TextSource = status.Text;
+
+                Text.Clear();
+
+                var img = new BitmapImage();
+                img.BeginInit();
+
+                img.UriSource =
+
+                new Uri(status.Author.ProfileImageUrl, UriKind.Absolute);
+                img.EndInit();
+
+                this.UserImage = img;
+                if (status.Entities == null)
+                {
+
+                    Text.Add(
+
+                    new TextSegment { Text = status.Text });
+                    return;
+                }
+
+                if (status.Entities.Any())
+                {
+
+                    var entities = status.Entities.OrderBy(x => x.StartIndex);
+                    Text.Add(
+
+                    new TextSegment() { Text = status.Text.Substring(0, entities.First().StartIndex) });
+                    foreach (var entity in entities)
+                    {
+
+                        Text.Add(
+
+                        new TextSegment { Text = status.Text.Substring(entity.StartIndex, entity.EndIndex - entity.StartIndex), IsHighlighted = true });
+                        entityIndex++;
+
+                        if (entities.Count() > entityIndex)
+                        {
+
+                            var nextEntity = entities.ElementAt(entityIndex);
+                            var
+                            distance = nextEntity.StartIndex - entity.EndIndex;
+                            Text.Add(
+
+                            new TextSegment() { Text = status.Text.Substring(entity.EndIndex, distance) });
+                        }
+
+                        else
+                        {
+
+                            Text.Add(
+
+                            new TextSegment() { Text = status.Text.Substring(entity.EndIndex, status.Text.Length - entity.EndIndex) });
+                        }
+
+                    }
+
+                }
+
+                else
+                {
+
+                    Text.Add(
+
+                    new TextSegment { Text = status.Text });
+                }
+
+            }
+
+        }
+
+        public class TextSegment
+        {
+
+            public String Text { get; set; }
+            public Boolean IsHighlighted { get; set; }
+        }
+
+        private TweetViewModel _currentTweet;
+        public TweetViewModel CurrentTweet
+        {
+
+            get { return this._currentTweet; }
+            set { ChangeAndNotify(ref this._currentTweet, value, () => CurrentTweet); }
+        }
+
+        public TViewModel(Dispatcher dispatcher)
+        {
+
+            this._queue = new CircularQueue<TweetViewModel>(10);
+            this._items = this._queue.GetEnumerator();
+            this._dispatcher = dispatcher;
+            AnonymousAccess();
+
+            AddInitialItems(10);
+
+            new DispatcherTimer(TimeSpan.FromSeconds(10.0), DispatcherPriority.Normal, Update, dispatcher);
+            new DispatcherTimer(TimeSpan.FromSeconds(15.0), DispatcherPriority.Normal, Retrieve, dispatcher);
+        }
+
+        private void AddInitialItems(Int32 count)
+        {
+
+            var results = this._service.Search("#CFLDraft", count, TwitterSearchResultType.Recent, (result, response) =>
+            {
+
+                foreach (var status in result.Statuses)
+                {
+
+                    this._dispatcher.BeginInvoke((Action)(() =>
+                    {
+
+                        var tweet = new TweetViewModel();
+                        tweet.SetData(status);
+
+                        this._queue.Add(tweet);
+                    }));
+
+                }
+
+                this._lastId = result.MaxId;
+            });
+
+        }
+
+        private void AnonymousAccess()
+        {
+
+            this._service = new TwitterService();
+            this._service.IncludeEntities = true;
+        }
+
+        private void Retrieve(Object sender, EventArgs e)
+        {
+
+            var results = this._service.SearchSince(this._lastId, "#CFLDraft", TwitterSearchResultType.Recent, (result, response) =>
+            {
+
+                foreach (var status in result.Statuses)
+                {
+
+                    this._dispatcher.BeginInvoke((Action)(() =>
+                    {
+
+                        var tweet = new TweetViewModel();
+                        tweet.SetData(status);
+
+                        this._queue.Add(tweet);
+                    }));
+
+                }
+
+                this._lastId = result.MaxId;
+            });
+
+        }
+
+        private void Update(Object sender, EventArgs e)
+        {
+
+            if (!_items.MoveNext())
+                return;
+            this.CurrentTweet = this._items.Current;
+        }
+    }
+}
